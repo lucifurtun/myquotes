97f9c3070c17b6c1f2eddfe9eef7f2ea876921d2
Luci Furtun <lucianfurtun@gmail.com>
2016-12-10 21:38:16 +0200

    Refactor urls

--- apps/quotes/urls.py
+++ apps/quotes/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    url(r'^dashboard/$', login_required(views.Dashboard.as_view()), name='dashboard'),
     url(r'^$', login_required(views.QuoteListView.as_view()), name='quotes_list'),
-    url(r'^quote/', login_required(views.QuoteView.as_view()), name='quotes_add'),
+    url(r'^quote/$', login_required(views.QuoteView.as_view()), name='quotes_add'),
+    url(r'^dashboard/$', login_required(views.Dashboard.as_view()), name='dashboard'),
 ]
--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,9 @@
-
 from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('apps.authentication.urls')),
     url(r'^api/', include('apps.api.urls')),
-    url(r'^', include('apps.quotes.urls', namespace='quotes')),
+    url(r'', include('apps.authentication.urls')),
+    url(r'', include('apps.quotes.urls', namespace='quotes')),
 ]
