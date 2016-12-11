44693f7570c76b3a9d4ef4266a010d8e61e00b21
Luci Furtun <lucianfurtun@gmail.com>
2016-12-11 22:11:35 +0200

    Add option to edit and delete quote

--- apps/quotes/urls.py
+++ apps/quotes/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', login_required(views.QuoteListView.as_view()), name='quotes_list'),
-    url(r'^quote/$', login_required(views.QuoteView.as_view()), name='quotes_add'),
+    url(r'^quote/(?P<pk>[0-9]+)/$', login_required(views.QuoteEditView.as_view()), name='quotes_edit'),
+    url(r'^quote/$', login_required(views.QuoteCreateView.as_view()), name='quotes_add'),
     url(r'^dashboard/$', login_required(views.Dashboard.as_view()), name='dashboard'),
 ]
--- apps/quotes/views.py
+++ apps/quotes/views.py
@@ -13,7 +13,14 @@ class Dashboard(generic.TemplateView):
     template_name = 'quotes/dashboard.html'
 
 
-class QuoteView(generic.CreateView):
+class QuoteCreateView(generic.CreateView):
+    template_name = 'quotes/quote_edit.html'
+    model = models.Quote
+    form_class = forms.QuoteForm
+    success_url = reverse_lazy('quotes:quotes_list')
+
+
+class QuoteEditView(generic.UpdateView):
     template_name = 'quotes/quote_edit.html'
     model = models.Quote
     form_class = forms.QuoteForm
