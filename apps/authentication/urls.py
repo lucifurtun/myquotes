1e8fbfaabbd91da33ca815a672f9c8952daf8af0
Luci Furtun <lucianfurtun@gmail.com>
2016-12-19 22:02:33 +0200

    Add user settings page

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^login/$', views.LoginView.as_view(), name='account_login'),
     url(r'^signup/$', views.SignUpView.as_view(), name='account_signup'),
     url(r'^logout/$', login_required(views.LogoutView.as_view()), name='account_logout'),
+    url(r'^settings/$', login_required(views.Settings.as_view()), name='account_settings'),
 ]
--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,6 +1,8 @@
 from allauth.account.views import LoginView as AllAuthLoginView
 from allauth.account.views import LogoutView as AllAuthLogoutView
 from allauth.account.views import SignupView as AllAuthSignupView
+from django.contrib.auth.models import User
+from django.views import generic
 
 
 class LoginView(AllAuthLoginView):
@@ -13,3 +15,12 @@ class LogoutView(AllAuthLogoutView):
 
 class SignUpView(AllAuthSignupView):
     template_name = 'authentication/signup.html'
+
+
+class Settings(generic.UpdateView):
+    model = User
+    template_name = 'authentication/user_edit.html'
+    fields = '__all__'
+
+    def get_object(self, queryset=None):
+        return self.request.user
