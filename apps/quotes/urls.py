f6db1c6fb32bd14656a6f50ccd0ae2b977c2838f
Luci Furtun <lucianfurtun@gmail.com>
2016-12-28 14:37:35 +0200

    Fix url regex

--- apps/quotes/urls.py
+++ apps/quotes/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^quote/(?P<pk>[0-9]+)/$', login_required(views.QuoteEditView.as_view()), name='quotes_edit'),
     url(r'^quote/$', login_required(views.QuoteCreateView.as_view()), name='quotes_add'),
     url(r'^dashboard/$', login_required(views.Dashboard.as_view()), name='dashboard'),
-    url(r'^(?P<slug>\w+)/$', views.UserQuoteListView.as_view(), name='profile'),
+    url(r'^(?P<slug>[\w.@+-]+)/$', views.UserQuoteListView.as_view(), name='profile'),
 ]
