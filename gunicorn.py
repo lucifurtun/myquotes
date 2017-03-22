7cc1effb69faf011a93d8f0471392ea19c542332
Luci Furtun <lucianfurtun@gmail.com>
2017-03-22 23:16:08 +0200

    Add gunicorn config

---
+++ gunicorn.py
@@ -0,0 +1,9 @@
+bind = 'unix:gunicorn.sock'
+workers = 1
+daemon = True
+pidfile = 'gunicorn.pid'
+loglevel = 'info'
+errorlog = 'gunicorn.error.log'
+logfile = 'gunicorn.access.log'
+accesslog = 'gunicorn.access.log'
+# access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
