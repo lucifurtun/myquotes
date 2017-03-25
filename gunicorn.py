5c983ac858bdf799a5945a13e76a1f9ddd7c6ad2
Luci Furtun <lucianfurtun@gmail.com>
2017-03-25 17:36:48 +0200

    Add automatic reloading for gunicorn

--- gunicorn.py
+++ gunicorn.py
@@ -1,9 +1,11 @@
 bind = 'unix:gunicorn.sock'
 workers = 1
+reload = True
 daemon = True
 pidfile = 'gunicorn.pid'
 loglevel = 'info'
 errorlog = 'gunicorn.error.log'
 logfile = 'gunicorn.access.log'
 accesslog = 'gunicorn.access.log'
+
 # access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
