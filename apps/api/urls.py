936c0c647e9f7d50e6767777547c16dc202e02c0
Luci Furtun <lucianfurtun@gmail.com>
2017-04-11 22:47:15 +0300

    Add refresh token functionality

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
-from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt import views as jwt_views
 
 from . import views
 
@@ -13,7 +13,8 @@ router.register(r'tags', views.TagViewSet)
 urlpatterns = [
     url(r'^docs/$', views.schema_view),
     url(r'^', include(router.urls)),
-    url(r'^token/', obtain_jwt_token),
+    url(r'^token/new/$', jwt_views.obtain_jwt_token),
+    url(r'^token/refresh/$', jwt_views.refresh_jwt_token),
     url(r'^filters/$', views.FiltersOptionsView.as_view()),
     url(r'^templates/(?P<page>[-\w]+.html)/$', views.AngularTemplateView.as_view()),
 ]
--- project/settings.py
+++ project/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import datetime
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
@@ -171,6 +173,11 @@ ACCOUNT_AUTHENTICATION_METHOD = 'email'
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
+
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=2),
+    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7)
+}
 
 CKEDITOR_CONFIGS = {
     "default": {
