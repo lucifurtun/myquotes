52702c00a7b3197f3242d6ae42af6fdea6aa61f1
Luci Furtun <lucianfurtun@gmail.com>
2017-04-12 09:38:02 +0300

    Explicitly set JWT_ALLOW_REFRESH to True

--- project/settings.py
+++ project/settings.py
@@ -176,7 +176,8 @@ LOGIN_REDIRECT_URL = '/'
 
 JWT_AUTH = {
     'JWT_EXPIRATION_DELTA': datetime.timedelta(days=2),
-    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7)
+    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
+    'JWT_ALLOW_REFRESH': True
 }
 
 CKEDITOR_CONFIGS = {
