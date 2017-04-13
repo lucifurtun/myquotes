03a54ea1920a3716e9c8d326f5c4c408f45b7d08
Luci Furtun <lucianfurtun@gmail.com>
2017-04-13 20:54:58 +0300

    Add option to verify jwt token

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -13,8 +13,9 @@ router.register(r'tags', views.TagViewSet)
 urlpatterns = [
     url(r'^docs/$', views.schema_view),
     url(r'^', include(router.urls)),
-    url(r'^token/new/$', jwt_views.obtain_jwt_token),
-    url(r'^token/refresh/$', jwt_views.refresh_jwt_token),
+    url(r'^token/new/$', jwt_views.ObtainJSONWebToken.as_view()),
+    url(r'^token/refresh/$', jwt_views.RefreshJSONWebToken.as_view()),
+    url(r'^token/verify/$', jwt_views.VerifyJSONWebToken.as_view()),
     url(r'^filters/$', views.FiltersOptionsView.as_view()),
     url(r'^templates/(?P<page>[-\w]+.html)/$', views.AngularTemplateView.as_view()),
 ]
