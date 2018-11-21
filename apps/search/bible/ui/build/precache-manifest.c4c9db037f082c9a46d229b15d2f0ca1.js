5384c6dbaaa6164d80f1e6d29b9aa37bd1deb16e
Luci Furtun <lucianfurtun@gmail.com>
2018-11-21 19:00:42 +0200

    Add a first build

---
+++ apps/search/bible/ui/build/precache-manifest.c4c9db037f082c9a46d229b15d2f0ca1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e39e460e9d313bac120d",
+    "url": "/bible/static/js/runtime~main.e39e460e.js"
+  },
+  {
+    "revision": "48b8e3c01cea37063398",
+    "url": "/bible/static/js/main.48b8e3c0.chunk.js"
+  },
+  {
+    "revision": "224a0f459bbcac307cb5",
+    "url": "/bible/static/js/1.224a0f45.chunk.js"
+  },
+  {
+    "revision": "48b8e3c01cea37063398",
+    "url": "/bible/static/css/main.318208b5.chunk.css"
+  },
+  {
+    "revision": "4e1a6b61dda01f920d7a950f910a8455",
+    "url": "/bible/index.html"
+  }
+];
