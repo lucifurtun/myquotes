8edd7be5824658dbe6f7f282573c3e7cce8b2586
Luci Furtun <lucianfurtun@gmail.com>
2019-01-02 22:18:04 +0200

    Update build

--- apps/search/bible/ui/build/service-worker.js
+++ apps/search/bible/ui/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bible/precache-manifest.12feeb8722ae142602cb996af3ab928e.js"
+  "/bible/precache-manifest.da840cf0c825cf3d136db1b026f1f8d2.js"
 );
 
 workbox.clientsClaim();
