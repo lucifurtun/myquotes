3500fba3c6aa31f423cfd5f93789f50346393f35
Luci Furtun <lucianfurtun@gmail.com>
2019-01-24 15:50:38 +0200

    Add missing file

---
+++ apps/search/bible/ui/build/precache-manifest.c088cc005023fe8333b6423a822d5dcf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e39e460e9d313bac120d",
+    "url": "/bible/static/js/runtime~main.e39e460e.js"
+  },
+  {
+    "revision": "e54f5efa6fc5d3cf45de",
+    "url": "/bible/static/js/main.e54f5efa.chunk.js"
+  },
+  {
+    "revision": "cac86de89bf13c202c87",
+    "url": "/bible/static/js/1.cac86de8.chunk.js"
+  },
+  {
+    "revision": "e54f5efa6fc5d3cf45de",
+    "url": "/bible/static/css/main.776ae5f4.chunk.css"
+  },
+  {
+    "revision": "2e010dcbe30827e3bac6b0a4e80f2639",
+    "url": "/bible/index.html"
+  }
+];
