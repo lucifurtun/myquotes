1e4ce15fdb1440375cb92e7db08403870b20ef49
Luci Furtun <lucianfurtun@gmail.com>
2019-02-02 19:32:11 +0200

    Add verse unique identifier

--- apps/search/bible/serializers.py
+++ apps/search/bible/serializers.py
@@ -8,16 +8,20 @@ from .models import Book, Chapter
 
 class VerseSerializer(DocumentSerializer):
     text_highlight = serializers.SerializerMethodField()
+    identifier = serializers.SerializerMethodField()
 
     class Meta:
         document = Verse
-        fields = ('id', 'number', 'text', 'chapter_number', 'book_title', 'highlight.text')
+        fields = ('id', 'number', 'text', 'book_title', 'book_number', 'chapter_number', 'highlight.text')
 
     def get_text_highlight(self, obj):
         if hasattr(obj.meta, 'highlight'):
             return obj.meta.highlight['text'][0]
 
         return None
+
+    def get_identifier(self, obj):
+        return f'{obj.book_number}_{obj.chapter_number}_{obj.number}'
 
 
 class ChapterSerializer(ModelSerializer):
