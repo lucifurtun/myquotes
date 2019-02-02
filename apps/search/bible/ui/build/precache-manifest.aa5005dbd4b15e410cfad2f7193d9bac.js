7adbb4adba69477cd4bcc5f90971c0498749ebf8
Luci Furtun <lucianfurtun@gmail.com>
2019-02-02 21:08:13 +0200

    Add build missing files

---
+++ apps/search/bible/ui/build/precache-manifest.aa5005dbd4b15e410cfad2f7193d9bac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e39e460e9d313bac120d",
+    "url": "/bible/static/js/runtime~main.e39e460e.js"
+  },
+  {
+    "revision": "3949f363c7a601a9a367",
+    "url": "/bible/static/js/main.3949f363.chunk.js"
+  },
+  {
+    "revision": "1e8c27e75058d619e127",
+    "url": "/bible/static/js/1.1e8c27e7.chunk.js"
+  },
+  {
+    "revision": "3949f363c7a601a9a367",
+    "url": "/bible/static/css/main.471db6f6.chunk.css"
+  },
+  {
+    "revision": "5683f0d7ac97a4a2aaec3964c14bd36f",
+    "url": "/bible/index.html"
+  }
+];
