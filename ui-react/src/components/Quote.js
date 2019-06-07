a6a8d18320e7485bf50980e4718fc4b54e9d1125
Luci Furtun <lucianfurtun@gmail.com>
2019-06-07 23:47:12 +0300

    Connect Quote to the store

--- ui-react/src/components/Quote.js
+++ ui-react/src/components/Quote.js
@@ -1,26 +1,37 @@
 import React from 'react'
 
 import { FaPencilAlt, FaTrashAlt } from 'react-icons/fa'
+import { connect } from 'react-redux'
+
 const html = (text) => ({ __html: text })
 
 
-const Quote = ({quote}) => (
+const Quote = ({ quote, user }) => (
     <div className="list-group">
         <div className="list-group-item">
-        <span className="quote-actions" ng-if="!readOnly">
-            <a href="" id="edit-button" className="visible-on-hover" ng-click="setEditItem(quote.id)">
-                <FaPencilAlt/>
-            </a>
-            <a href="" id="delete-button" className="visible-on-hover"
-               ng-click="setDeleteItem(quote.id, quote.title)">
-                <FaTrashAlt/>
-            </a>
-        </span>
-            <h4 className="list-group-item-heading">{quote.title}</h4>
-            <p className="list-group-item-text" dangerouslySetInnerHTML={html(quote.text)}></p>
-            <div className="author label label-danger">{quote.author_name}</div>
+            { (quote.author.id === user.id) &&
+            <span className="quote-actions" ng-if="!readOnly">
+                <a href="" id="edit-button" className="visible-on-hover" ng-click="setEditItem(quote.id)">
+                    <FaPencilAlt/>
+                </a>
+                <a href="" id="delete-button" className="visible-on-hover"
+                   ng-click="setDeleteItem(quote.id, quote.title)">
+                    <FaTrashAlt/>
+                </a>
+            </span>
+            }
+            <h4 className="list-group-item-heading">{ quote.title }</h4>
+            <p className="list-group-item-text" dangerouslySetInnerHTML={ html(quote.text) }></p>
+            <div className="author label label-danger">{ quote.author_name }</div>
         </div>
     </div>
 )
 
-export default Quote
+
+function mapStateToProps(state) {
+    return {
+        user: state.user
+    }
+}
+
+export default connect(mapStateToProps)(Quote)
