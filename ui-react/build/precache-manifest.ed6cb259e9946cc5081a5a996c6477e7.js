79a758d4b9cf2ef9f06c4d5394a1f87c7ddefbba
Luci Furtun <lucianfurtun@gmail.com>
2019-10-18 18:03:06 +0300

    Add UI build

---
+++ ui-react/build/precache-manifest.ed6cb259e9946cc5081a5a996c6477e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44eb217d7ae349429c49c6332c5787e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f455d46d5cf5ab6ac2d",
+    "url": "/static/css/main.a057f9a8.chunk.css"
+  },
+  {
+    "revision": "1dc98e3c572b4463b948",
+    "url": "/static/js/2.4760567d.chunk.js"
+  },
+  {
+    "revision": "2f455d46d5cf5ab6ac2d",
+    "url": "/static/js/main.44796ace.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
