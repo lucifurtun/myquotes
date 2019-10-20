c50ada3c270acd2cbd8e3c2ab04353a8e3359d97
Luci Furtun <lucianfurtun@gmail.com>
2019-10-20 23:08:49 +0300

    Update build

---
+++ ui-react/build/precache-manifest.29859fc23363df0d3f59adb7502766da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73720ba38a7512520bb5c30390755257",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d3f67469aeb56cff5cd",
+    "url": "/static/css/main.28685793.chunk.css"
+  },
+  {
+    "revision": "02fa40a6ad969f52d921",
+    "url": "/static/js/2.45b2bffa.chunk.js"
+  },
+  {
+    "revision": "3d3f67469aeb56cff5cd",
+    "url": "/static/js/main.1a2446dc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
