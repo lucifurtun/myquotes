aae087fc65455792a4b3b0f3a3a5be35fc756a8a
Luci Furtun <lucianfurtun@gmail.com>
2020-05-27 16:30:15 +0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,4 +2,5 @@
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 docker-compose exec db pg_dump --no-owner --no-acl -U myquotes myquotes > db.dump
-pipenv run python drive.py
+workon backups
+python drive.py
