8e26ad23a7b16f6b2d197ea103785476bfc802f8
Luci Furtun <lucianfurtun@gmail.com>
2020-06-15 23:48:31 +0300

    Perform some upgrades

---
+++ apps/search/bible/ui/build/precache-manifest.bbbb68c91d204f2ec315730d12fc0f47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d021e7e1bb49224459829c84e4b4139",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca78bf3483ea654f5322",
+    "url": "/static/css/main.40082966.chunk.css"
+  },
+  {
+    "revision": "d0c2deb21b6d9fd35501",
+    "url": "/static/js/2.080fae0f.chunk.js"
+  },
+  {
+    "revision": "9474f6265372615b79437968091ba056",
+    "url": "/static/js/2.080fae0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca78bf3483ea654f5322",
+    "url": "/static/js/main.6c76d84d.chunk.js"
+  },
+  {
+    "revision": "3e72ed575e9bfa148b09",
+    "url": "/static/js/runtime-main.c4adb1c0.js"
+  }
+]);
