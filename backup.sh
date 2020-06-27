be554aa65eb7eb60bf0f31b1eacaadbb04dc3892
Luci Furtun <lucianfurtun@gmail.com>
2020-06-27 22:22:21 +0300

    Fix backup.sh

--- backup.sh
+++ backup.sh
@@ -2,6 +2,5 @@
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 docker-compose exec -T db pg_dump --no-owner --no-acl -U myquotes myquotes > db.dump
-source ~/Envs/backups/bin/activate
-python drive.py
+python3 drive.py
 mv db.dump db_old.dump
