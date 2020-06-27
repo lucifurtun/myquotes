ac4f0e22dbda5820134b56c8cafe6d24d1157f29
Luci Furtun <lucianfurtun@gmail.com>
2020-06-27 22:23:08 +0300

    Remove node_modules from static files dirs

--- project/settings.py
+++ project/settings.py
@@ -146,7 +146,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "ui"),
-    os.path.join(BASE_DIR, "node_modules"),
 ]
 
 AUTHENTICATION_BACKENDS = (
